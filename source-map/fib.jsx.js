d4d3ee332148ddb6a3d6e60d944d96e8e817227d
Fuji, Goro <g.psy.va@gmail.com>
2012-08-26 12:38:36 +0900

    update

--- source-map/fib.jsx.js
+++ source-map/fib.jsx.js
@@ -83,6 +83,12 @@ JSX.postProfileResults = function (url) {
 		throw new Error("profiler has not been turned on");
 	return $__jsx_profiler.postResults(url);
 };
+
+JSX.resetProfileResults = function () {
+	if ($__jsx_profiler.resetResults == null)
+		throw new Error("profiler has not been turned on");
+	return $__jsx_profiler.resetResults();
+};
 /**
  * class Fib extends Object
  * @constructor
